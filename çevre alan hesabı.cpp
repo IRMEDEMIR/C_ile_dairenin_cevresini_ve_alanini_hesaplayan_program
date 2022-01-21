461dcddf9d05b22a1240680a12b4e17da0b2e0c4
İrem Demir <95531247+IRMEDEMIR@users.noreply.github.com>
2022-01-21 18:39:53 +0300

    Create çevre alan hesabı.cpp
    <conio.h> kütüphanesi kullanılarak çevre alan hesabı

---
+++ çevre alan hesabı.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main(){
+	float pi=3.14 ;
+	float yaricap , cevre , alan;
+	printf("yaricapi giriniz:");
+	scanf("%f",&yaricap);
+	
+	cevre= (2*pi*yaricap);
+	alan= pi*yaricap*yaricap;
+	printf("yaricap:%.2f, cevre:%.2f, alan:%.2f",yaricap,cevre,alan);
+	getch();
+	return 0;
+}
